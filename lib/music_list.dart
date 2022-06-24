9316df3b69a397ab5cf46e7903dd14cc1f1b9bb6
Joy Sameza <samgaming500@gmail.com>
2022-06-24 21:51:28 +0100

    12

--- lib/music_list.dart
+++ lib/music_list.dart
@@ -13,6 +13,7 @@ class _MusicListState extends State<MusicList> {
   Widget build(BuildContext context) {
     const mainColor = Colors.grey;
     const backColor = Colors.white;
+    const thirdColor = Color.fromARGB(255, 122, 85, 129);
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     double size = ((width + height) / 2) * 0.075;
@@ -31,17 +32,21 @@ class _MusicListState extends State<MusicList> {
             ),
             actions: [
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  return;
+                },
                 icon: const Icon(Icons.shuffle_outlined),
                 tooltip: 'Shuffle',
               ),
               const SizedBox(width: 6.0),
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.repeat_on_rounded)),
               Padding(
                 padding: const EdgeInsets.only(right: 10.0),
                 child: IconButton(
                   onPressed: () {},
-                  icon: const Icon(Icons.repeat_on),
-                  tooltip: 'Repeat',
+                  icon: const Icon(Icons.more_vert_rounded),
+                  tooltip: 'Option',
                 ),
               )
             ]),
@@ -83,15 +88,16 @@ class _MusicListState extends State<MusicList> {
                           backgroundColor: mainColor,
                         ),
                         title: Text(
+                          overflow: TextOverflow.ellipsis,
                           "Music ${index + 1}",
                           style: const TextStyle(
                               fontSize: 20.0, fontWeight: FontWeight.bold),
                         ),
                         subtitle: Text(
                           "Author ${index + 1}",
+                          overflow: TextOverflow.ellipsis,
                           style:
                               const TextStyle(fontSize: 14.0, color: mainColor),
-                          textDirection: TextDirection.ltr,
                         ),
                       ),
                     ),
@@ -105,53 +111,70 @@ class _MusicListState extends State<MusicList> {
                   child: Align(
                     alignment: Alignment.bottomCenter,
                     child: ElevatedButton(
-                      onPressed: () => null,
+                      onPressed: () {},
                       style: ElevatedButton.styleFrom(
                         elevation: 10.0,
-                        primary: mainColor,
+                        primary: thirdColor,
                         minimumSize: Size((width * 0.8), 80),
                         maximumSize: Size((width), 100),
                         shape: const StadiumBorder(),
                         shadowColor: mainColor,
                       ),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           const CircleAvatar(
-                            radius: 20.0,
+                            radius: 25.0,
                             backgroundColor: backColor,
                           ),
                           const SizedBox(
                             width: 8.0,
                           ),
-                          Expanded(child: ListView(
-                            children: [
-                              Text(
-                                'Hight: $height',
-                                textAlign: TextAlign.center,
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1,
-                                style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, letterSpacing: 2.0),
-                              ),
-                              Text('Width: $width dddd dddddddddddddddefgh', 
-                                textAlign: TextAlign.center,
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1,
-                                style: const TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, letterSpacing: 1.0,
-                                color: Color.fromARGB(255, 40, 40, 40)),)
-                            ],
-                          ),
+                          Expanded(
+                            child: ListView(
+                              children: [
+                                Text(
+                                  'Hight: $height',
+                                  textAlign: TextAlign.center,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 2.0),
+                                ),
+                                Text(
+                                  'Width: $width dddd dddddddddddddddefgh',
+                                  textAlign: TextAlign.center,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12.0,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 1.0,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                           IconButton(
                               onPressed: () {},
-                              icon: const Icon(Icons.skip_previous),iconSize: size),
+                              icon: const Icon(Icons.skip_previous_rounded),
+                              color: Colors.white,
+                              iconSize: size),
                           IconButton(
                             onPressed: () {},
-                            icon: const Icon(Icons.play_arrow),
+                            icon: const Icon(Icons.play_arrow_rounded),
+                            color: Colors.white,
                             iconSize: size,
                           ),
                           IconButton(
                               onPressed: () {},
-                              icon: const Icon(Icons.skip_next),iconSize: size),
+                              icon: const Icon(Icons.skip_next_rounded),
+                              color: Colors.white,
+                              iconSize: size),
                         ],
                       ),
                     ),
--- lib/play_screen.dart
+++ lib/play_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:music_player/music_list.dart';
 
 class PlayScreen extends StatefulWidget {
-  const PlayScreen({Key? key, required this.title, required this.author}) : super(key: key);
+  const PlayScreen({Key? key, required this.title, required this.author})
+      : super(key: key);
   final String title;
   final String author;
 
@@ -15,93 +16,134 @@ class _PlayScreenState extends State<PlayScreen> {
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('GDSC Music Player'),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const MusicList())
-            );
-          }, 
-          icon: const Icon(Icons.expand_more_outlined)),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 10.0),
-            child: Icon(Icons.more_vert),
-          ),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 40.0),
-              child: Container(
-                height: 200.0,
-                width: 200.0,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(170, 160, 160, 160),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Color.fromARGB(211, 255, 255, 255), Color.fromARGB(255, 122, 85, 129)])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          centerTitle: true,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const MusicList()));
+              },
+              icon: const Icon(Icons.expand_more_outlined)),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Container(
+                  height: 200.0,
+                  width: 200.0,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(170, 160, 160, 160),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 40.0),
-            child: Column(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 40.0),
+              child: Column(
                 children: [
                   Text(widget.title,
-                  overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
                       )),
                   Text(widget.author,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold, 
-                      fontSize: 16,
-                      color: Color.fromARGB(170, 160, 160, 160))),
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Color.fromARGB(170, 49, 49, 49))),
                 ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 200.0, 10.0, 0.0),
+              child: Row(
+                children: [
+                  const Text("00:00", style: TextStyle(color: Colors.white),),
+                  Expanded(
+                    child: Slider.adaptive(
+                      activeColor: Colors.white,
+                      thumbColor: Colors.white,
+                      inactiveColor: const Color.fromARGB(255, 175, 175, 175),
+                      value: _currentValue,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _currentValue = newValue;
+                        });
+                      }),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 10.0),
+                    child: Text("00:00", style: TextStyle(color: Colors.white),),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.shuffle_rounded),
+                  iconSize: 50.0,
+                  color: Colors.white,
                 ),
-          ),
-              const Text("00:00"),
-              
-              Slider.adaptive(
-                  value: _currentValue,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _currentValue = newValue;
-                    });
-                  }),
-              const Text("00:00"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {},
-                iconSize: 50.0,
-                icon: const Icon(Icons.skip_previous),
-              ),
-              IconButton(
-                onPressed: () {},
-                iconSize: 50.0,
-                icon: const Icon(Icons.play_arrow),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.skip_next),
-                iconSize: 50.0,
-              ),
-            ],
-          ),
-        ],
+                IconButton(
+                  onPressed: () {},
+                  iconSize: 50.0,
+                  color: Colors.white,
+                  icon: const Icon(Icons.skip_previous_rounded),
+                ),
+                SizedBox(
+                  width: (MediaQuery.of(context).size.width) * 0.01,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  iconSize: 50.0,
+                  color: Colors.white,
+                  icon: const Icon(Icons.play_arrow_rounded),
+                ),
+                SizedBox(
+                  width: (MediaQuery.of(context).size.width) * 0.01,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  icon: const Icon(Icons.skip_next_rounded),
+                  iconSize: 50.0,
+                ),
+                SizedBox(
+                  width: (MediaQuery.of(context).size.width) * 0.01,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.repeat_rounded),
+                  color: Colors.white,
+                  iconSize: 50.0,
+                ),
+              ],
+            ),
+            const Expanded(
+              child: SizedBox(
+
+              ),),
+          ],
+        ),
       ),
     );
   }
